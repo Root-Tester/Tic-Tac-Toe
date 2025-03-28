e4b6f321b4687c5834b6090db3c858a5fccc9444
Sung Jinhwoo <tusharsahu757@gmail.com>
2025-03-28 12:43:07 +0530

    Update Tic-Tac-Toe2.js

--- Tic-Tac-Toe2.js
+++ Tic-Tac-Toe2.js
@@ -1,3 +1,11 @@
+/*!
+ * Palindrome Checker Web App
+ * Copyright (c) 2025 Tushar Kumar Sahu
+ * Licensed under the Apache License 2.0
+ * See LICENSE file for more information.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 const board = new Array(9).fill("");
 let opponant = null;
 let home = null;
